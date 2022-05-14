a111be7eb3f6e4b9fb4febb329e4b3f5d4493c37
Michael Millis <mmillis1@gmail.com>
2022-05-14 12:20:08 -0700

    bring in breeze and reformat

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,18 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class UserFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
+class UserFactory extends Factory {
+
+    public function definition(): array {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
@@ -26,13 +17,7 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return static
-     */
-    public function unverified()
-    {
+    public function unverified(): static {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/groups', function () {
+        return view('groups');
+    })->name('groups');
+});
+
+
+require __DIR__.'/auth.php';
